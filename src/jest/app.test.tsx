5728b1419bcdf682e0af47134ad8cefdde692fd0
sobao <ryota.kono@mazrica.com>
2022-10-21 18:57:20 +0900

    不要なテストをコメントアウトした

---
+++ src/jest/app.test.tsx
@@ -0,0 +1,9 @@
+// import * as React from "react";
+// import { render } from "@testing-library/react";
+// import "@testing-library/jest-dom/extend-expect";
+// import { App } from "../components/templates/app";
+
+// test("mounted text", () => {
+//   const { getByTestId } = render(<App></App>);
+//   expect(getByTestId("text")).toHaveTextContent("hello world.");
+// });
