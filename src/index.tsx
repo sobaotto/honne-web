b35eb2c0b9de40a1f33d253c3c4a2e01978aac43
sobao <ryota.kono@mazrica.com>
2022-10-21 19:00:15 +0900

    resset.cssを適用した

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app";
 import "./i18n/i18n";
+import "./styles/reset.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
