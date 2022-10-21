b0108ae75d4839c1a643c3a3146d7800582661dc
sobao <ryota.kono@mazrica.com>
2022-10-21 18:59:49 +0900

    i18nextを導入した

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,22 +1,20 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import translationJA from './locales/ja/translation.json'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationJA from "./locales/ja/translation.json";
 
 const resources = {
-    ja: {
-        translation: translationJA,
-    }
+  ja: {
+    translation: translationJA,
+  },
 };
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: "ja",
-        debug: true,
-        interpolation: {
-            escapeValue: false,
-        }
-    });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "ja",
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app";
-import './i18n/i18n';
+import "./i18n/i18n";
 
 ReactDOM.render(<App />, document.getElementById("root"));
